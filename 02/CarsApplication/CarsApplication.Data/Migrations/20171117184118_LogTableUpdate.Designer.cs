337857b4a21d6b1b8ead5fe9e5784a953dca44c2
M-Yankov <cska31ultras@gmail.com>
2017-11-18 03:18:43 +0200

    Added migrations for Logs table

--- 02/CarsApplication/CarsApplication.Data/Migrations/20171117184118_LogTableUpdate.Designer.cs
+++ 02/CarsApplication/CarsApplication.Data/Migrations/20171117184118_LogTableUpdate.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace CarsApplication.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20171117184118_LogTableUpdate")]
+    partial class LogTableUpdate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ 02/CarsApplication/CarsApplication.Data/Migrations/20171117184118_LogTableUpdate.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace CarsApplication.Data.Migrations
+{
+    public partial class LogTableUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Logs",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Logs_UserId",
+                table: "Logs",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Logs_AspNetUsers_UserId",
+                table: "Logs",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Logs_AspNetUsers_UserId",
+                table: "Logs");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Logs_UserId",
+                table: "Logs");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Logs");
+        }
+    }
+}
